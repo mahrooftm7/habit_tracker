6cbe1b6af399eae78223bf39c45059ac88e83ec1
mahrooftm7 <mahrooftm@gmail.com>
2026-09-01 22:40:03 +0530

    Clean unused imports in auth_screen.dart

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:url_launcher/url_launcher.dart';
 import '../models/user.dart';
 import '../services/auth_service.dart';
 import '../services/supabase_service.dart';
