d6745d1f6bfc80cf2112320e660382613d4dff9d
mahrooftm7 <mahrooftm@gmail.com>
2026-09-01 19:50:33 +0530

    Update Android package name to com.tymat.habittracker and label to TYM Habit Tracker

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.habit_tracker"
+    namespace = "com.tymat.habittracker"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -15,8 +15,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.habit_tracker"
+        applicationId = "com.tymat.habittracker"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
---
+++ android/app/src/main/kotlin/com/tymat/habittracker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.tymat.habittracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
